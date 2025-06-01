71c422fd6cdb7887f01ea7edec2f344ab93a0633
Jon-Felipe <rodriguesjf7@gmail.com>
2025-06-01 09:47:36 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// components
+import Header from './components/Header';
+import Login from './components/Login';
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
